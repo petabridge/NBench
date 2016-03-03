90ca2604099432f8faf3cff01f58f8eee4ca7ec6
corneliutusnea <corneliu@acorns.com.au>
2016-03-03 18:21:07 +1100

    #62 Added support to include/exclude tests from the benchmark

--- tests/NBench.Tests.End2End/NBenchIntregrationTest.cs
+++ tests/NBench.Tests.End2End/NBenchIntregrationTest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Petabridge <https://petabridge.com/>. All rights reserved.
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
 using System.Linq;
 using NBench.Reporting;
 using NBench.Reporting.Targets;
@@ -39,16 +40,57 @@ namespace NBench.Tests.End2End
         [Fact]
         public void LoadAssemblyCorrect()
         {
+	        var package = LoadPackage();
+	        var result = TestRunner.Run(package);
+            Assert.True(result.AllTestsPassed);
+			Assert.NotEqual(0, result.ExecutedTestsCount);
+			Assert.Equal(0, result.IgnoredTestsCount);
+		}
+
+		
+	    [Fact]
+		public void RunnerIncludeInvalidName()
+		{
+			var package = LoadPackage(new string[] { "unknown" });
+
+			var result = TestRunner.Run(package);
+			Assert.True(result.AllTestsPassed);
+			Assert.Equal(0, result.ExecutedTestsCount);
+			Assert.NotEqual(0, result.IgnoredTestsCount);
+		}
+
+		[Fact]
+		public void RunnerIncludePattern()
+		{
+			var package = LoadPackage(new [] { "*.ConfigBenchmark*" });
+
+			var result = TestRunner.Run(package);
+			Assert.True(result.AllTestsPassed);
+			Assert.Equal(1, result.ExecutedTestsCount);
+			Assert.NotEqual(0, result.IgnoredTestsCount);
+		}
+		[Fact]
+		public void RunnerExcludePattern()
+		{
+			var package = LoadPackage(null, new [] { "*CounterThroughputBenchmark*", "*SimpleCounterBenchmark*" });
+
+			var result = TestRunner.Run(package);
+			Assert.True(result.AllTestsPassed);
+			Assert.Equal(1, result.ExecutedTestsCount);
+			Assert.NotEqual(0, result.IgnoredTestsCount);
+		}
+
+		private static TestPackage LoadPackage(IEnumerable<string> include = null, IEnumerable<string> exclude = null)
+		{
 #if DEBUG
-	var package = new TestPackage(@"..\..\..\NBench.Tests.Assembly\bin\Debug\NBench.Tests.Assembly.dll");
+	var package = new TestPackage(@"..\..\..\NBench.Tests.Assembly\bin\Debug\NBench.Tests.Assembly.dll", include, exclude);
 #else
-    var package = new TestPackage(@"..\..\..\NBench.Tests.Assembly\bin\Release\NBench.Tests.Assembly.dll"); 
+			var package = new TestPackage(@"..\..\..\NBench.Tests.Assembly\bin\Release\NBench.Tests.Assembly.dll", include, exclude);
 #endif
 
 			package.Validate();
-
-			Assert.True(TestRunner.Run(package));
-        }
-    }
+			return package;
+		}
+	}
 }
 
