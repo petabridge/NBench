bc0c502066ea3b34916d76084180295f10a3a541
Maxim Salamatko <maxim.salamatko@gmail.com>
2016-12-21 09:31:11 +0400

    #159 updated BenchmarkDotNet

--- benchmarks/NBench.Microbenchmarks/Util/Util_AtomicCounters.cs
+++ benchmarks/NBench.Microbenchmarks/Util/Util_AtomicCounters.cs
@@ -2,25 +2,26 @@
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 
 using BenchmarkDotNet;
-using BenchmarkDotNet.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
 using NBench.Util;
 
 namespace NBench.Microbenchmarks.Util
 {
-    [BenchmarkTask(platform:BenchmarkPlatform.X64, jitVersion: BenchmarkJitVersion.LegacyJit)]
-    [BenchmarkTask(platform: BenchmarkPlatform.X86, jitVersion: BenchmarkJitVersion.LegacyJit)]
-    [BenchmarkTask(platform: BenchmarkPlatform.X64, jitVersion: BenchmarkJitVersion.RyuJit)]
+    [LegacyJitX64Job]
+    [LegacyJitX86Job]
+    [RyuJitX64Job]
     public class Util_AtomicCounters
     {
         private readonly AtomicCounter _counter = new AtomicCounter();
 
-        [Benchmark("Count how quickly we can increment AtomicCounter")]
+        [Benchmark(Description = "Count how quickly we can increment AtomicCounter")]
         public void Increment()
         {
             _counter.Increment();
         }
 
-        [Benchmark("Count how quickly we can increment and get AtomicCounter")]
+        [Benchmark(Description = "Count how quickly we can increment and get AtomicCounter")]
         public long GetAndIncrement()
         {
             return _counter.GetAndIncrement();
