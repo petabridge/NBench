fd63cb86487cec5fe7512989a9bcf287b8bb6177
Aaron Stannard <aaron@petabridge.com>
2018-07-10 08:27:41 -0500

    added NBench.props and NBench.targets; added 2.1 .NET Core SDK

--- build.ps1
+++ build.ps1
@@ -32,7 +32,7 @@ Param(
 $FakeVersion = "4.61.2"
 $NBenchVersion = "1.0.1"
 $DotNetChannel = "LTS";
-$DotNetVersion = "2.0.0";
+$DotNetVersion = "2.1.301";
 $DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/v$DotNetVersion/scripts/obtain/dotnet-install.ps1";
 $NugetVersion = "4.1.0";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/v$NugetVersion/nuget.exe"
