2dd0f98a421526c51c71a1ed88d8029ef2f5aace
Aaron Stannard <aaron@petabridge.com>
2020-02-21 15:03:31 -0600

    made NBench.Tests.Performance into a console application

---
+++ tests/NBench.Tests.Performance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBench.Tests.Performance
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return NBenchRunner.Run<Program>();
+        }
+    }
+}
